3221652e20b7e57fd000c4f00c6c479b9d359352
Sadiq Hussain <sa7434542@gmail.com>
2025-11-03 15:29:56 +0500

    new website book cover design

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from './componets/common/Navbar';
+import Footer from './componets/common/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+
+
+const MainLayout = () => {
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+      <Outlet /> 
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+     element: <MainLayout />,
+     children: [
+      { index: true, element: <Home /> },
+      { index: true, element: <About /> },
+     
+     ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
