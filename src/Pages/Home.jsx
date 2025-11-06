5695bfe57a6150ad61478789d0551dd64b327ef9
Sadiq Hussain <sa7434542@gmail.com>
2025-11-06 18:19:26 +0500

    solve error in component home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Herosection from "../components/home/HeroSection";
 // import { Mail, BookOpen, Heart } from "lucide-react";
-import Herosection from "../components/Home/HeroSection";
-import AboutBook from "../components/Home/AboutBook";
-import AboutAuthor from "../components/Home/AboutAuthor";
-import FeatureSection from "../components/Home/FeatureSection";
-import Testimonials from "../components/Home/Testimonials";
-import Newsletter from "../components/Home/Newsletter";
-import BuySection from "../components/Home/BuySection";
-import BookBenefits from "../components/Home/BookBenefits"
-import WhoShouldRead from "../components/Home/WhoShouldRead";
+import AboutBook from "../components/home/AboutBook";
+import AboutAuthor from "../components/home/AboutAuthor";
+import FeatureSection from "../components/home/FeatureSection";
+import Testimonials from "../components/home/Testimonials";
+import Newsletter from "../components/home/Newsletter";
+import BuySection from "../components/home/BuySection";
+import BookBenefits from "../components/home/BookBenefits"
+import WhoShouldRead from "../components/home/WhoShouldRead";
 
 const Home = () => {
   return (
     <div className=" mt-20 font-sans bg-[#f9f6f2] text-gray-800">
       <Herosection/>
+      
       <AboutBook/>
       <FeatureSection/>
       <BookBenefits/>
