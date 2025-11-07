fd24155aa7196400b8a6eedc7fe7cb03ccf287ae
Sadiq Hussain <sa7434542@gmail.com>
2025-11-07 11:17:05 +0500

    update imports of about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import AboutHero from '../components/about/AboutHero';
-import BookIntroduction from '../components/about/BookIntro';
-import KeyFeatures from '../components/about/KeyFeatures';
-import TargetAudience from '../components/about/TargetAudience';
-import DesignPhilosophy from '../components/about/DesignPhilosophy';
-import CTASection from '../components/about/CTASection';
+import React from "react";
+import AboutHero from "../components/About/AboutHero";
+import BookIntroduction from "../components/About/BookIntro";
+import KeyFeatures from "../components/About/KeyFeatures";
+import TargetAudience from "../components/About/TargetAudience";
+import DesignPhilosophy from "../components/About/DesignPhilosophy";
+import CTASection from "../components/About/CTASection";
 
 function About() {
   return (
     <>
       <div>
-        <AboutHero/>
-        <BookIntroduction/>
-        <KeyFeatures/>
-        <TargetAudience/>
-        <DesignPhilosophy/>
-        <CTASection/>
+        <AboutHero />
+        <BookIntroduction />
+        <KeyFeatures />
+        <TargetAudience />
+        <DesignPhilosophy />
+        <CTASection />
       </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
